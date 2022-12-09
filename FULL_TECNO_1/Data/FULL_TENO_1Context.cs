411c95b924816c71b8359b44028cf20ea968028a
JUNIOR <junioralberto1421@gmail.com>
2022-12-08 19:38:20 -0600

    1

---
+++ FULL_TECNO_1/Data/FULL_TENO_1Context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FULL_TECNO_1.Data
+{
+    public class FULL_TENO_1Context : DbContext
+    {
+        public FULL_TENO_1Context(DbContextOptions<FULL_TENO_1Context> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Audifonos> Audifonos { get; set; }
+        public DbSet<Monitores> Monitores { get; set; }
+        public DbSet<Mouses> Mouses { get; set; }
+        public DbSet<Procesadores> Procesadores { get; set; }
+        public DbSet<TarjetasGraficas> TarjetasGraficas { get; set; }
+    }
+}
